d0ddfcbe69344809794869ef88224eac564d0ad2
Achieng45 <stephanieachieng45@gmail.com>
2024-02-28 23:44:46 +0300

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'learning_angular';
+  title = 'learning angular';
+  isdisabled=false
+  onClick=true
 }

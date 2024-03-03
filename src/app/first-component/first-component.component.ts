cc6465717f7499c6cf2e63273ff5e61d02a31ad6
Achieng45 <stephanieachieng45@gmail.com>
2024-03-04 00:59:22 +0300

    communication btn components

--- src/app/first-component/first-component.component.ts
+++ src/app/first-component/first-component.component.ts
@@ -1,19 +1,25 @@
-import { NgClass } from '@angular/common';
+import { FormStyle, NgClass, NgStyle } from '@angular/common';
 import { withInterceptors } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-first-component',
   standalone: true,
-  imports: [RouterOutlet,NgClass],
+  imports: [RouterOutlet,NgClass,NgStyle,FormsModule,NgModel],
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
 
 export class FirstComponentComponent {
+  isStyleApplied=false
+
+
   highlightcolor= 'grey';
   highlight(color: string){
     this.highlightcolor=color;
 }
-
+togglestyle(){
+  this.isStyleApplied=!this.isStyleApplied
+}
 }
